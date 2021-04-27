853e728be9b69bd470bca1a2f8fee6d1397e0bec
Helen <sang.s.yi@gmail.com>
2021-04-27 00:04:43 -0700

    portfolio links and images

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,20 +1,38 @@
 import React from "react";
+import runBuddy from "../../assets/images/run-buddy.png"
+import weatherDash from "../../assets/images/weather.png"
+import dayScheduler from "../../assets/images/day-scheduler.png"
+import horiseon from "../../assets/images/horiseon.png"
+import safePassage from "../../assets/images/safe-passage.png"
+import cornstocks from "../../assets/images/cornstocks.png"
 
 const Projects = () => (
   <div>
-    <h1>Blog Page</h1>
-    <p>
-      Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-      turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-      Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-      nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi justo
-      laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad litora
-      torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-      lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec placerat
-      accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque vehicula sit
-      amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus non ullamcorper
-      iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
-    </p>
+    <h1>Portfolio</h1>
+    <div>
+      <a href="https://sangyi05.github.io/run-buddy/"><img src={runBuddy}></img></a>
+      <a href="https://github.com/sangyi05/run-buddy">Run Buddy GitHub</a>
+    </div>
+    <div>
+      <a href="https://sangyi05.github.io/weather-dashboard/"><img src={weatherDash}></img></a>
+      <a href="https://github.com/sangyi05/weather-dashboard">Weather Dashboard GitHub</a>
+    </div>
+    <div>
+      <a href="https://sangyi05.github.io/day-scheduler/"><img src={dayScheduler}></img></a>
+      <a href="https://github.com/sangyi05/day-scheduler">Day Scheduler GitHub</a>
+    </div>
+    <div>
+      <a href="https://sangyi05.github.io/horiseon-marketing/"><img src={horiseon}></img></a>
+      <a href="https://github.com/sangyi05/horiseon-marketing">Horiseon Marketing GitHub</a>
+    </div>
+    <div>
+      <a href="https://jaderiver62.github.io/safe-passage/"><img src={safePassage}></img></a>
+      <a href="https://github.com/jaderiver62/safe-passage">Safe Passage GitHub</a>
+    </div>
+    <div>
+      <a href="https://cornstocks-10-team.herokuapp.com/"><img src={cornstocks}></img></a>
+      <a href="https://github.com/subin-su/Project02-Requirements">Cornstocks GitHub</a>
+    </div>
   </div>
 );
 
