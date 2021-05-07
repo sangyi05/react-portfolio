c53daf250ab2b4397e725fadce9083a9d72fd3a5
Helen <sang.s.yi@gmail.com>
2021-05-07 00:02:55 -0700

    colors

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -10,27 +10,33 @@ const Projects = () => (
   <div>
     <h1>Portfolio</h1>
     <div>
-      <a href="https://sangyi05.github.io/run-buddy/"><img src={runBuddy} className="img-thumbnail mx-1"></img></a>
+      <img src={runBuddy}></img>
+      <a href="https://sangyi05.github.io/run-buddy/">Run Buddy Website</a>
       <a href="https://github.com/sangyi05/run-buddy">Run Buddy GitHub</a>
     </div>
     <div>
-      <a href="https://sangyi05.github.io/weather-dashboard/"><img src={weatherDash} className="img-thumbnail mx-1"></img></a>
+      <img src={weatherDash}></img>
+      <a href="https://sangyi05.github.io/weather-dashboard/">Weather Dashboard Website</a>
       <a href="https://github.com/sangyi05/weather-dashboard">Weather Dashboard GitHub</a>
     </div>
     <div>
-      <a href="https://sangyi05.github.io/day-scheduler/"><img src={dayScheduler} className="img-thumbnail mx-1"></img></a>
+      <img src={dayScheduler}></img>
+      <a href="https://sangyi05.github.io/day-scheduler/">Day Scheduler Website</a>
       <a href="https://github.com/sangyi05/day-scheduler">Day Scheduler GitHub</a>
     </div>
     <div>
-      <a href="https://sangyi05.github.io/horiseon-marketing/"><img src={horiseon} className="img-thumbnail mx-1"></img></a>
+      <img src={horiseon}></img>
+      <a href="https://sangyi05.github.io/horiseon-marketing/">Horiseon Marketing Website</a>
       <a href="https://github.com/sangyi05/horiseon-marketing">Horiseon Marketing GitHub</a>
     </div>
     <div>
-      <a href="https://jaderiver62.github.io/safe-passage/"><img src={safePassage} className="img-thumbnail mx-1"></img></a>
+      <img src={safePassage}></img>
+      <a href="https://jaderiver62.github.io/safe-passage/">Safe Passage Website</a>
       <a href="https://github.com/jaderiver62/safe-passage">Safe Passage GitHub</a>
     </div>
     <div>
-      <a href="https://cornstocks-10-team.herokuapp.com/"><img src={cornstocks} className="img-thumbnail mx-1"></img></a>
+      <img src={cornstocks}></img>
+      <a href="https://cornstocks-10-team.herokuapp.com/">Cornstocks Website</a>
       <a href="https://github.com/subin-su/Project02-Requirements">Cornstocks GitHub</a>
     </div>
   </div>
